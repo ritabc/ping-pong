28a8ac0d71843caa8f6248fb07c7d5ae452147ce
Rita Bennett-Chew and Will Swanson <rita@>
2018-06-25 10:02:33 -0700

    add failing test 3

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -9,4 +9,7 @@ describe('#ping_pong') do
   it("replaces multiples of 3 with 'ping'") do
     expect(ping_pong(3)).to(eq([1,2,'ping']))
   end
+  it("replaces multiples of 5 with 'pong'") do
+    expect(ping_pong(5)).to(eq([1,2,'ping',4,'pong']))
+  end
 end
