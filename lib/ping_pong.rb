73a42a336d7c78ba3f447e207d0b8f0e4b44f035
Rita Bennett-Chew and Will Swanson <rita@>
2018-06-25 10:00:18 -0700

    add test 2 passing

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,4 +1,11 @@
 def ping_pong(number)
-  initial_array = Array(1..number)
+  initial_array = []
+  (1..number).each do |increment|
+    if increment == 3
+      initial_array.push("ping")
+    else
+      initial_array.push(increment)
+    end
+  end
   initial_array
 end
